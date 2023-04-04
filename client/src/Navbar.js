e6c3623a53256c88c28b540455f854e33cf9caba
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2023-04-04 15:48:21 -0700

    navbar

---
+++ client/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+    <nav>
+        <ul>
+        <li><a href="Home">Home</a></li>
+        <li><a href="About">About</a></li>
+        <li><a href="Upcoming">Upcoming Events</a></li>
+    </ul>
+    </nav>
+    );
+}
+
+export default Navbar;
---
+++ database/app/controllers/attendees_controller.rb
@@ -0,0 +1,52 @@
+class AttendeesController < ApplicationController
+  before_action :set_attendee, only: %i[ show update destroy ]
+
+  # GET /attendees
+  def index
+    @attendees = Attendee.all
+
+    render json: @attendees
+  end
+
+  # GET /attendees/1
+  def show
+    @antendee = Attendee.find(params[:id])
+    render json: @attendee
+  end
+
+  # POST /attendees
+  def create
+    @attendee = Attendee.new(attendee_params)
+
+    if @attendee.save
+      render json: @attendee, status: :created
+    else
+      render json: @attendee.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /attendees/1
+  def update
+    if @attendee.update(attendee_params)
+      render json: @attendee
+    else
+      render json: @attendee.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /attendees/1
+  def destroy
+    @attendee.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_attendee
+      @attendee = Attendee.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def attendee_params
+      params.permit(:UserID, :EventID)
+    end
+end
