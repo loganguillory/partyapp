689eb67940bcecb71fa403a8734b7302974ff612
alantmatos <mr.alanmatos@hotmail.com>
2024-02-03 12:36:03 -0500

    Create card

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import './index.css';
+import imgPizza from "./temp/pizzaparty.jpg";
 
 function Home() {
+
     return (<div className='home'>
 
         <div>
             <h2>Hi There, Are you on your way?</h2>
             <h4>Upcoming Events:</h4>
-            <h5>See Events for more.</h5>
+            <h5 className="eventslink"><a href="/events">See Events for more</a></h5>
         </div>
 
 
         <div className="card-container">
                 <div className="card">
-                    <img src="path/to/your/photo1.jpg" alt="Event Card" />
+                    <img src={imgPizza} alt="Event Card" />
                     <div className="card-content">
-                        <div className="card-title">Card Title 1</div>
+                        <div className="card-title">Jason's Pizza Party </div>
                         <div className="card-description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
-                            probare,
-                            quae sunt a te dicta?
+                        Indulge in a delightful evening of flavor at our Pizza 
+                        Party! Join us for a mouthwatering celebration featuring 
+                        a variety of artisanal pizzas, each crafted with the finest 
+                        ingredients and baked to perfection. 
                         </div>
-                        <div className="card-location">Location: City, Country</div>
+                        <div className="card-location">Location: Brooklyn, NY</div>
                     </div>
                 </div>
         </div>
